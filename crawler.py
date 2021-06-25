47b06a47c021fcd630cf48d0554fbf4a3299c189
Vitor <vitorascorrea@gmail.com>
2021-06-25 16:28:24 -0300

    Fix double quotes

--- crawler.py
+++ crawler.py
@@ -22,7 +22,7 @@ def get_random_search_term():
 
 def set_old_user_agent(driver):
   stealth(driver,
-          user_agent='DN',
+          user_agent="DN",
           languages=["en-US", "en"],
           vendor="Google Inc.",
           platform="Win32",
@@ -34,7 +34,7 @@ def set_old_user_agent(driver):
 
 def set_new_user_agent(driver):
   stealth(driver,
-          user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
+          user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36",
           languages=["en-US", "en"],
           vendor="Google Inc.",
           platform="Win32",
@@ -46,15 +46,15 @@ def set_new_user_agent(driver):
 
 if __name__ == "__main__":
   options = Options()
-  options.add_argument('--allow-running-insecure-content')
+  options.add_argument("--allow-running-insecure-content")
   options.add_argument("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36")
-  options.add_experimental_option("excludeSwitches", ['enable-logging', 'enable-automation'])
-  options.add_experimental_option('useAutomationExtension', False)
-  options.add_argument('--disable-blink-features=AutomationControlled')
+  options.add_experimental_option("excludeSwitches", ["enable-logging", "enable-automation"])
+  options.add_experimental_option("useAutomationExtension", False)
+  options.add_argument("--disable-blink-features=AutomationControlled")
 
   search_key_history = []
 
-  driver = webdriver.Chrome(options=options, executable_path='./chromedriver')
+  driver = webdriver.Chrome(options=options, executable_path="./chromedriver")
 
   # this switch of user agents was based from here: https://www.reddit.com/r/Python/comments/mtvbz1/selenium_google_login_blocked_in_automation/
   set_old_user_agent(driver)
@@ -62,7 +62,7 @@ if __name__ == "__main__":
   driver.get("http://google.com/ncr")
   sleep(1)
   # new tab opening inspired from here: https://python-forum.io/thread-7530.html
-  driver.execute_script("window.open('');")
+  driver.execute_script("window.open("");")
   sleep(3)
   driver.switch_to.window(driver.window_handles[1])
   sleep(1)
@@ -82,10 +82,10 @@ if __name__ == "__main__":
     sleep(5)
 
     try:
-      element = driver.find_element_by_partial_link_text('I want to play')
-      print('Found link. Login in the first tab and after that press anything here and ENTER')
+      element = driver.find_element_by_partial_link_text("I want to play")
+      print("Found link. Login in the first tab and after that press anything here and ENTER")
       print(search_key_history)
-      a = input()
+      wait_input = input()
       break
     except:
       pass
